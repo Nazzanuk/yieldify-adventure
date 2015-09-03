d79b01d0ed7d054701ad6929602989a9c4aee258
Nathan N <nazzanuk@gmail.com>
2015-09-03 02:01:01 +0100

    css fixes, removed redundantbower files

--- gulpfile.js
+++ gulpfile.js
@@ -46,8 +46,6 @@ gulp.task("gen-js-lib", function(){
     return gulp.src([
         'src/bower-components/jquery/dist/jquery.min.js',
         'src/bower-components/angular/angular.min.js',
-        'src/bower-components/velocity/velocity.min.js',
-        'src/bower-components/velocity/velocity.ui.min.js',
         'src/bower-components/underscore/underscore-min.js'
     ])
         .pipe(concat('lib.js'))
